7d7e81e93ce113f05357c04ec90fc38359acd994
Mistyttm <emmey.leo@gmail.com>
2023-04-08 17:08:36 +1000

    Create navbar css

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import logo from "../logo.svg";
 
 function Navbar() {
 
 
     return (
         <div class="navbar-bg">
+            {/* <img src={logo} alt="website logo"/> */}
             <nav class="navlinks">
                 <ul>
                     <li>
@@ -13,7 +15,7 @@ function Navbar() {
                     <li>
                         <Link to="/Movies">Movies</Link>
                     </li>
-                    <li>
+                    <li id="login">
                         <Link to="/Login">Login/Register</Link>
                     </li>
                 </ul>
