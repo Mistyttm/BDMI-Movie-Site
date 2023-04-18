fd62eb8b84ae8609db050b7bb815e8b062bc5a35
Mistyttm <emmey.leo@gmail.com>
2023-04-18 14:36:32 +1000

    Add the api call beginnings

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,91 +1,83 @@
 import { React, useCallback, useMemo, useState } from "react";
-import { AgGridReact } from 'ag-grid-react';
+import { AgGridReact } from "ag-grid-react";
 import { useSearchParams } from "react-router-dom";
-import Navbar from '../components/navbar';
+import Navbar from "../components/navbar";
 import Footer from "../components/footer";
-import "ag-grid-community/styles/ag-grid.css"
-import "ag-grid-community/styles/ag-theme-alpine.css"
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 
+const sampleFetch = async () => {
+    const response = await fetch(
+        "http://sefdb02.qut.edu.au:3000/movies/search"
+    ); // hits the url provided and gets the response with its status code, body, headers, etc.
+    const jsonData = await response.json(); // parses the response body to JSON
 
+    console.log(jsonData); // all the data
+    console.log(jsonData.pagination); // prints the value of the property
+    console.log(jsonData.pagination.total);
+    // ... etc
+
+    return jsonData.pagination.total;
+};
+
+const GridExample = () => {};
 
 function Movies() {
     const [queryParameters] = useSearchParams();
-    
-    const GridExample = () => {
-        const containerStyle = useMemo(() => ({ width: '100%', height: '100%' }), []);
-        const gridStyle = useMemo(() => ({ height: '100%', width: '100%' }), []);
 
-        const [columnDefs, setColumnDefs] = useState([
-            // this row shows the row index, doesn't use any data from the row
-            {
-                headerName: 'ID',
-                maxWidth: 100,
-                // it is important to have node.id here, so that when the id changes (which happens
-                // when the row is loaded) then the cell is refreshed.
-                valueGetter: 'node.id',
-                cellRenderer: (props) => {
+    const containerStyle = useMemo(
+        () => ({ width: "100%", height: "100%" }),
+        []
+    );
+    const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
+
+    const [columnDefs, setColumnDefs] = useState([
+        // this row shows the row index, doesn't use any data from the row
+        { headerName: "Title", 
+            field: "title", 
+            minWidth: 150, 
+            // it is important to have node.id here, so that when the id changes (which happens
+            // when the row is loaded) then the cell is refreshed.
+            valueGetter: "node.id",
+            cellRenderer: (props) => {
                 if (props.value !== undefined) {
                     return props.value;
                 } else {
                     return (
-                    <img src="https://www.ag-grid.com/example-assets/loading.gif" />
+                        <img src="https://www.ag-grid.com/example-assets/loading.gif" alt="loading" />
                     );
                 }
-                },
-            },
-            { field: 'athlete', minWidth: 150 },
-            { field: 'age' },
-            { field: 'country', minWidth: 150 },
-            { field: 'year' },
-            { field: 'date', minWidth: 150 },
-            { field: 'sport', minWidth: 150 },
-            { field: 'gold' },
-            { field: 'silver' },
-            { field: 'bronze' },
-            { field: 'total' },
-            ]);
-            const defaultColDef = useMemo(() => {
-            return {
-                flex: 1,
-                resizable: true,
-                minWidth: 100,
-            };
-            }, []);
-        
-            const onGridReady = useCallback((params) => {
-                const dataSource = {
-                    rowCount: undefined,
-                    getRows: (params) => {
-                        console.log(
-                            'asking for ' + params.startRow + ' to ' + params.endRow
-                        );
-                        // At this point in your code, you would call the server.
-                        // To make the demo look real, wait for 500ms before returning
-                        setTimeout(function () {
-                            // take a slice of the total rows
-                            const rowsThisPage = data.slice(params.startRow, params.endRow);
-                            // if on or after the last page, work out the last row.
-                            let lastRow = -1;
-                            if (data.length <= params.endRow) {
-                                lastRow = data.length;
-                            }
-                            // call the success callback
-                            params.successCallback(rowsThisPage, lastRow);
-                        }, 500);
-                    },
-                };
-            },
-        []);
+            }, 
+        },
+        { headerName: "Year", field: "year" },
+        { headerName: "IMDB Rating", field: "imdbRating" },
+        { headerName: "Rotten Tomatoes Rating", field: "rottenTomatoesRating" },
+        { headerName: "MetaCritic Rating", field: "metacriticRating"},
+        { headerName: "Classification", field: "classification" },
+    ]);
+    const defaultColDef = useMemo(() => {
+        return {
+            flex: 1,
+            resizable: true,
+            minWidth: 100,
+        };
+    }, []);
 
-
-    const columns= [
-        {headerName: "Title", field: "title", sortable: true, filter: true},
-        {headerName: "Year", field: "year", sortable: true, filter: 'agNumberColumnFilter'},
-        {headerName: "IMDB Rating", field: "imdbRating", sortable: true, filter: 'agNumberColumnFilter'},
-        {headerName: "Rotten Tomatoes Rating", field: "rottenTomatoesRating", sortable: true, filter: 'agNumberColumnFilter'},
-        {headerName: "MetaCritic Rating", field: "metacriticRating", sortable: true, filter: 'agNumberColumnFilter'},
-        {headerName: "Classification", field: "classification", sortable: true, filter: true},
-    ];
+    const onGridReady = useCallback((params) => {
+        const dataSource = {
+            rowCount: undefined,
+            getRows: async (params) => {
+                console.log(
+                    'asking for ' + params.startRow + ' to ' + params.endRow
+                );
+                
+                
+                const pageTotal = await sampleFetch();
+                console.log(pageTotal);
+            },
+        };
+        params.api.setDatasource(dataSource);
+    }, []);
 
     let movieValue = "";
 
@@ -96,13 +88,15 @@ function Movies() {
     return (
         <div className="App">
             <Navbar />
-            <div className="ag-theme-alpine" style={{ height: "600px", width: "1200px" }}>
+            <div
+                className="ag-theme-alpine"
+                style={{ height: "600px", width: "1200px" }}>
                 <AgGridReact
                     columnDefs={columnDefs}
                     defaultColDef={defaultColDef}
                     rowBuffer={0}
-                    rowSelection={'multiple'}
-                    rowModelType={'infinite'}
+                    rowSelection={"multiple"}
+                    rowModelType={"infinite"}
                     cacheBlockSize={100}
                     cacheOverflowSize={2}
                     maxConcurrentDatasourceRequests={1}
