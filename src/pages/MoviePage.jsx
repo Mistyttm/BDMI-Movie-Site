901273027a862565ee8ac370080b1be328cf6b05
Mistyttm <emmey.leo@gmail.com>
2023-04-26 00:03:38 +1000

    begin work on individual movie page

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-import Navbar from '../components/navbar';
+import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 
 function Movie() {
     const [queryParameters] = useSearchParams();
+    const apiURL =
+        "http://sefdb02.qut.edu.au:3000/movies/data/" +
+        queryParameters.get("m");
+
+    const [movieData, setMovieData] = useState({});
+
+    const getApiData = async () => {
+        const response = await fetch(apiURL)
+            .then((response) => response.json())
+            .then((response) => setMovieData(response));
+    };
+
+    useEffect(() => {
+        getApiData();
+    }, []);
 
     const navigate = useNavigate();
-    console.log();
-    useEffect(() => {document.title = queryParameters.get("m")}, []);
+    useEffect(() => {
+        document.title = movieData.title;
+    }, [movieData]);
 
     return (
         <div className="App">
             <Navbar />
-            <p>Movie Test</p>
+            <p>{movieData.title}</p>
+            <p>{movieData.year}</p>
+            <p>{movieData.runtime}</p>
+            <p>{movieData.country} minutes</p>
             <Footer />
         </div>
     );
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -5,15 +5,19 @@ import { useSearchParams, useNavigate } from "react-router-dom";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import paginationTotal from "../components/paginationTotal";
-import CountStatusBarComponent from "../components/countStatusBarComponent"
+import CountStatusBarComponent from "../components/countStatusBarComponent";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import "../Styles/Movies/Movies.css";
 
 function Movies(props) {
-    useEffect(() => {document.title = props.title}, []);
+    useEffect(() => {
+        document.title = props.title;
+    }, []);
     const [queryParameters] = useSearchParams();
+    const [requestError, setRequestError] = useState(null);
     const navigate = useNavigate();
     let pageCount = 0;
 
@@ -24,13 +28,12 @@ function Movies(props) {
     const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
 
     const [columnDefs, setColumnDefs] = useState([
-        // this row shows the row index, doesn't use any data from the row
         {
-            headerName: "ID",
-            maxWidth: 100,
-            // it is important to have node.id here, so that when the id changes (which happens
-            // when the row is loaded) then the cell is refreshed.
-            valueGetter: "node.id",
+            headerName: "Title",
+            field: "title",
+            minWidth: 150,
+            maxWidth: 500,
+            resizable: true,
             cellRenderer: (props) => {
                 if (props.value !== undefined) {
                     return props.value;
@@ -41,13 +44,32 @@ function Movies(props) {
                 }
             },
         },
-        { headerName: "Title", field: "title", minWidth: 150, maxWidth: 400, resizable: true },
         { headerName: "Year", field: "year", maxWidth: 100, resizable: false },
-        { headerName: "IMDB Rating", field: "imdbRating", maxWidth: 130, resizable: false },
-        { headerName: "Rotten Tomatoes Rating", field: "rottenTomatoesRating", maxWidth: 200, resizable: false },
-        { headerName: "MetaCritic Rating", field: "metacriticRating", maxWidth: 150, resizable: false },
-        { headerName: "Classification", field: "classification", maxWidth: 130, resizable: false },
-        { headerName: "IMDB ID", field: "imdbID", hide: true},
+        {
+            headerName: "IMDB Rating",
+            field: "imdbRating",
+            maxWidth: 130,
+            resizable: false,
+        },
+        {
+            headerName: "Rotten Tomatoes Rating",
+            field: "rottenTomatoesRating",
+            maxWidth: 200,
+            resizable: false,
+        },
+        {
+            headerName: "MetaCritic Rating",
+            field: "metacriticRating",
+            maxWidth: 150,
+            resizable: false,
+        },
+        {
+            headerName: "Classification",
+            field: "classification",
+            maxWidth: 130,
+            resizable: false,
+        },
+        { headerName: "IMDB ID", field: "imdbID", hide: true },
     ]);
     const defaultColDef = useMemo(() => {
         return {
@@ -58,26 +80,34 @@ function Movies(props) {
     }, []);
 
     const onRowSelected = useCallback((event) => {
-        navigate("/MoviePage?m=" + event.data.imdbID)
+        navigate("/MoviePage?m=" + event.data.imdbID);
     }, []);
 
     const onGridReady = useCallback((params) => {
         const dataSource = {
             rowCount: undefined,
             getRows: async (params) => {
-                const pageTotal = await paginationTotal();
+                //const pageTotal = await paginationTotal();
                 pageCount += 1;
 
                 let movieValue = "";
 
                 if (queryParameters.get("globalSearch")) {
-                    movieValue = "?title=" + queryParameters.get("globalSearch");
+                    movieValue =
+                        "?title=" + queryParameters.get("globalSearch");
                 } else {
                     movieValue = "?page=" + pageCount;
                 }
-                fetch("http://sefdb02.qut.edu.au:3000/movies/search" + movieValue)
+                fetch(
+                    "http://sefdb02.qut.edu.au:3000/movies/search" + movieValue
+                )
                     .then((res) => res.json())
-                    .then((data) => params.successCallback(data.data, -1));
+                    .then((data) => params.successCallback(data.data, -1))
+                    .catch((err) =>
+                        setRequestError(
+                            "404: Movies could not be found at this time. Please try again later."
+                        )
+                    );
             },
         };
         params.api.setDatasource(dataSource);
@@ -87,34 +117,41 @@ function Movies(props) {
         return {
             statusPanels: [
                 {
-                statusPanel: CountStatusBarComponent,
+                    statusPanel: CountStatusBarComponent,
                 },
-            ],  
+            ],
         };
     }, []);
-    
 
     return (
         <div className="App">
             <Navbar />
-            <div
-                className="ag-theme-alpine"
-                style={{ height: "600px", width: "1200px" }}>
-                <AgGridReact
-                    columnDefs={columnDefs}
-                    defaultColDef={defaultColDef}
-                    rowBuffer={0}
-                    rowSelection={"multiple"}
-                    rowModelType={"infinite"}
-                    cacheBlockSize={100}
-                    cacheOverflowSize={2}
-                    maxConcurrentDatasourceRequests={1}
-                    infiniteInitialRowCount={1000}
-                    maxBlocksInCache={10}
-                    onGridReady={onGridReady}
-                    onRowSelected={onRowSelected}
-                    statusBar={statusBar}
-                />
+            <div className="gridContainer">
+                {requestError != null ? (
+                    <p className="requestError">
+                        <FontAwesomeIcon icon={faCircleExclamation} /> Error{" "}
+                        {requestError}
+                    </p>
+                ) : null}
+                <div
+                    className="ag-theme-alpine"
+                    style={{ height: "600px", width: "1200px" }}>
+                    <AgGridReact
+                        columnDefs={columnDefs}
+                        defaultColDef={defaultColDef}
+                        rowBuffer={0}
+                        rowSelection={"multiple"}
+                        rowModelType={"infinite"}
+                        cacheBlockSize={100}
+                        cacheOverflowSize={2}
+                        maxConcurrentDatasourceRequests={1}
+                        infiniteInitialRowCount={1000}
+                        maxBlocksInCache={10}
+                        onGridReady={onGridReady}
+                        onRowSelected={onRowSelected}
+                        statusBar={statusBar}
+                    />
+                </div>
             </div>
             <Footer />
         </div>
