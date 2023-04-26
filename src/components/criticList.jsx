68ad9e0cf55869537b73ba6ce91617e7173b992b
Mistyttm <emmey.leo@gmail.com>
2023-04-26 23:44:56 +1000

    Simplify criticList component and add comments

--- src/components/criticList.jsx
+++ src/components/criticList.jsx
@@ -1,43 +1,45 @@
-function Critic(props){
-    let badgeClass = "badge0";
+function Critic(props) {
+    let badgeClass = "badge0"; // Initialize badgeClass to "badge0"
 
-    if (props.source === "Internet Movie Database"){
-        if (props.value <= 3){
-            badgeClass = "badge1";
-        }
-        if (props.value <= 8 && props.value > 3){
-            badgeClass = "badge2";
-        }
-        if (props.value <= 10 && props.value > 8){
-            badgeClass = "badge3";
-        }
-    }
-    if (props.source === "Rotten Tomatoes"){
-        if (props.value <= 25){
-            badgeClass = "badge1";
-        }
-        if (props.value <= 50 && props.value > 25){
-            badgeClass = "badge2";
-        }
-        if (props.value <= 100 && props.value > 50){
-            badgeClass = "badge3";
-        }
-    }
-    if (props.source === "Metacritic"){
-        if (props.value <= 40){
-            badgeClass = "badge1";
-        }
-        if (props.value <= 75 && props.value > 40){
-            badgeClass = "badge2";
-        }
-        if (props.value <= 100 && props.value > 75){
-            badgeClass = "badge3";
+    // Check if the value prop is not 0
+    if (props.value !== 0) {
+        // Determine badgeClass based on source and value props
+        if (props.source === "Internet Movie Database") {
+            if (props.value <= 3) {
+                badgeClass = "badge1";
+            } else if (props.value <= 8) {
+                badgeClass = "badge2";
+            } else {
+                badgeClass = "badge3";
+            }
+        } else if (props.source === "Rotten Tomatoes") {
+            if (props.value <= 25) {
+                badgeClass = "badge1";
+            } else if (props.value <= 50) {
+                badgeClass = "badge2";
+            } else {
+                badgeClass = "badge3";
+            }
+        } else if (props.source === "Metacritic") {
+            if (props.value <= 40) {
+                badgeClass = "badge1";
+            } else if (props.value <= 75) {
+                badgeClass = "badge2";
+            } else {
+                badgeClass = "badge3";
+            }
         }
     }
 
-    return(
+    // Render the component with appropriate badgeClass and source prop
+    return (
         <div className={props.source}>
-            <p><b className="titler">{props.source}</b>: <div className={badgeClass}><p>{props.value}</p></div></p>
+            <p>
+                <b className="titler">{props.source}</b>:{" "}
+                <div className={badgeClass}>
+                    <p>{props.value}</p>
+                </div>
+            </p>
         </div>
     );
 }
