9da609e5e988d34ff26e9988577e7c4c19721316
Mistyttm <emmey.leo@gmail.com>
2023-05-13 15:54:12 +1000

    Generic Commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { useEffect } from "react";
-import RegisterBox from '../components/registerBox';
-import Navbar from '../components/navbar';
+import RegisterBox from "../components/registerBox";
+import Navbar from "../components/navbar";
 import Footer from "../components/footer";
-import '../Styles/Register/Register.css';
-import '../Styles/Register/RegisterMedia.css'
+import "../Styles/Register/Register.css";
+import "../Styles/Register/RegisterMedia.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+Aos.init();
 
 function Register(props) {
-    useEffect(() => {document.title = props.title}, []);
+    useEffect(() => {
+        document.title = props.title;
+    }, []);
     return (
         <div className="App">
             <Navbar />
-            <RegisterBox />
+            <div data-aos="flip-right">
+                <RegisterBox />
+            </div>
             <Footer />
         </div>
     );
